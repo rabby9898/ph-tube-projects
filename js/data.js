0b593c6f2ca2f73b150f6c215c3beec7c22f4d97
rabby9898 <mdfajlerabby00@gmail.com>
2023-09-01 02:16:14 +0600

    no data found section added

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ const loadCategoryData = async () => {
   );
   const data = await res.json();
   const categoryData = data.data;
-  //   console.log(categoryData);
+  console.log(categoryData);
   displayCategory(categoryData);
 };
 
@@ -14,6 +14,7 @@ const displayCategory = (categoryData) => {
     const div = document.createElement("div");
     div.innerHTML = `
     <button
+            onclick="handleCategory('${category.category_id}')"
             class="btn btn-ghost bg-[#D2D4D7] text-xs md:text-base capitalize mr-4 hover:bg-[#FF1F3D] active:bg-[#FF1F3D] focus:outline-none focus:bg-[#FF1F3D]"
           >
             ${category.category}
@@ -23,9 +24,9 @@ const displayCategory = (categoryData) => {
   });
 };
 
-const loadVideoData = async () => {
+const loadVideoData = async (id) => {
   const res = await fetch(
-    `https://openapi.programming-hero.com/api/videos/category/1000`
+    `https://openapi.programming-hero.com/api/videos/category/${id}`
   );
   const data = await res.json();
   const videoData = data.data;
@@ -35,36 +36,75 @@ const loadVideoData = async () => {
 
 const displayVideoData = (videoData) => {
   const cardAreaElement = document.getElementById("card-area");
+  cardAreaElement.innerHTML = "";
+
+  if (videoData.length === 0) {
+    noDataFound();
+    return;
+  }
   videoData.forEach((item) => {
+    let totalMs = item.others.posted_date;
+    console.log(totalMs);
+    // console.log(new Date(totalMs).toISOString());
     const div = document.createElement("div");
-    div.classList = `card w-full lg:w-96 bg-base-100 shadow-xl my-6`;
+    div.classList = `card w-full md:w-[280px] lg:w-96 bg-base-100 shadow-xl my-6`;
 
     div.innerHTML = `
-      <figure>
-          <img
-            class="w-full h-64"
-              src=${item.thumbnail}
-          />
-      </figure>
-      <div class="card-body">
-          <h2 class="card-title">${item.title}</h2>
-          <div class="flex items-center">
+        <figure>
             <img
-            class="rounded-full w-14 h-14 mr-3"
-                src=${item.authors[0].profile_picture}
+              class="w-full h-64 relative"
+                src=${item.thumbnail}
             />
-            <p>${item.authors[0].profile_name} ${
+            <p class="bg-black text-white absolute top-56 left-64">${
+              item.others.posted_date
+            } ago</p>
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">${item.title}</h2>
+            <div class="flex items-center">
+              <img
+              class="rounded-full w-14 h-14 mr-3"
+                  src=${item.authors[0].profile_picture}
+              />
+              <p>${item.authors[0].profile_name} ${
       item.authors[0].verified
         ? '<i class="fa-solid fa-certificate text-[#2568EF] "></i>'
         : ""
     }</p>
-          </div>
-          <p class="ml-16">${item.others.views} views</p>
-      </div>
-    `;
+            </div>
+            <p class="ml-16">${item.others.views} views</p>
+        </div>
+      `;
     cardAreaElement.appendChild(div);
   });
+  clearData();
+};
+
+/********** No Data Found Area Function **********/
+const noDataFound = () => {
+  const noDataArea = document.getElementById("no-data");
+  noDataArea.innerHTML = "";
+  const div = document.createElement("div");
+  div.classList = `text-center`;
+  div.innerHTML = `
+    <img
+      class="text-center mx-auto"
+      src='/images/Icon.png'
+    />
+    <h1 class="text-black font-semibold text-4xl text-center mt-3">Oops!! Sorry, There is no <br> content here</h1>
+    `;
+  noDataArea.appendChild(div);
+};
+
+const clearData = () => {
+  const noDataArea = document.getElementById("no-data");
+  noDataArea.innerHTML = "";
+};
+
+const handleCategory = (id) => {
+  loadVideoData(id);
 };
 
 loadCategoryData();
-loadVideoData();
+
+loadVideoData("1000");
